1dc163678b97212ebde2c8d05bc610d861fe9e97
Andrei Pufu <a.c.pufu@gmail.com>
2024-05-29 07:44:56 +0300

    added luarocks/pulseaudio_dbus to pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,10 @@
 # You can build them using 'nix build .#example'
 {pkgs, ...}: {
   # example = pkgs.callPackage ./example { };
+
+  # pkgs
   sddm-themes = pkgs.callPackage ./sddm-themes.nix {};
+
+  # luarocks
+  luarocks_pulseaudio_dbus = pkgs.callPackage ./luarocks-pkgs/pulseaudio_dbus.nix {};
 }
