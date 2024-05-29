cff220564f4988cbaeea3d00b4b0efea9f20cdaa
Andrei Pufu <a.c.pufu@gmail.com>
2024-05-29 08:59:39 +0300

    fixed formatting in pkgs/lua/enum/default.nix

--- pkgs/lua/enum/default.nix
+++ pkgs/lua/enum/default.nix
@@ -1,26 +1,26 @@
-{ lua, buildLuaPackage, fetchFromGitHub }:
-let
-
+{
+  lua,
+  buildLuaPackage,
+  fetchFromGitHub,
+}: let
   pname = "enum";
-
 in
-buildLuaPackage rec {
-  version = "0.1.1";
-  name = "${pname}-${version}";
-  inherit pname;
+  buildLuaPackage rec {
+    version = "0.1.1";
+    name = "${pname}-${version}";
+    inherit pname;
 
-  src = fetchFromGitHub {
-    owner = "stefano-m";
-    repo = "lua-${pname}";
-    rev = "v${version}";
-    sha256 = "1lz0v1sihxxlnn0z2x3wv8gr6xg4q5pw7da2x9hh9cgqgz4v4xib";
-  };
+    src = fetchFromGitHub {
+      owner = "stefano-m";
+      repo = "lua-${pname}";
+      rev = "v${version}";
+      sha256 = "1lz0v1sihxxlnn0z2x3wv8gr6xg4q5pw7da2x9hh9cgqgz4v4xib";
+    };
 
-  buildPhase = ":";
+    buildPhase = ":";
 
-  installPhase = ''
-    mkdir -p "$out/share/lua/${lua.luaversion}"
-    cp -r src/${pname} "$out/share/lua/${lua.luaversion}/"
-  '';
-
-}
+    installPhase = ''
+      mkdir -p "$out/share/lua/${lua.luaversion}"
+      cp -r src/${pname} "$out/share/lua/${lua.luaversion}/"
+    '';
+  }
