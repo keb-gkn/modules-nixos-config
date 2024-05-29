49a6c9d3608d4b29d3ea2aa70a54194a3ad09925
Andrei Pufu <a.c.pufu@gmail.com>
2024-05-29 17:19:56 +0300

    changed sddm theme revision

--- pkgs/sddm-themes.nix
+++ pkgs/sddm-themes.nix
@@ -12,8 +12,8 @@
     src = fetchFromGitHub {
       owner = "CloudyChris";
       repo = "nord-sddm";
-      rev = "c05d462bf44459896665fd8661cd7b45fe4759e3";
-      hash = "sha256-4CQN2oBCqwJPCmUroL7bFu2Rc5N2hVHStKdSwonnGw0=";
+      rev = "accc4f03cb4f72d75ab1e0f6a103f82bc1090402";
+      hash = "sha256-Gt+l8SBU/xDy5Sg+cR4OgWS0A+bv28HOFfTx9Zql5jk=";
     };
   };
 }
