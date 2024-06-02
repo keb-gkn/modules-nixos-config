10c9baf3adc5789cabf8293c2c12bb02b800c999
Andrei Pufu <a.c.pufu@gmail.com>
2024-06-02 14:05:26 +0300

    added file_exists and readlines to filesystem tools

--- home-manager/awesome/src/tools/filesystem_utils.lua
+++ home-manager/awesome/src/tools/filesystem_utils.lua
@@ -3,6 +3,27 @@ local lfs = require("lfs")
 -- directory: string -- the location to walk through
 -- recursive: bool -- should or should not walk subdirs :: default false
 -- mode: string -- [a, f, d] :: default a
+function file_exists(file)
+    local f = io.open(file, "rb")
+    if f then
+        f:close()
+    end
+    return f ~= nil
+end
+
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+function readlines(file)
+    if not file_exists(file) then
+        return {}
+    end
+    local lines = {}
+    for line in io.lines(file) do
+        lines[#lines + 1] = line
+    end
+    return lines
+end
+
 function walk(self, directory, recursive, mode)
     recursive = recursive or false
     mode = mode or "a"
