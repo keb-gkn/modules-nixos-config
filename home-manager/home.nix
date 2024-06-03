8bff1992571070113c44e23f482d32165341bd3b
CloudyChris <a.c.pufu@gmail.com>
2024-06-04 01:32:52 +0300

    nordic

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -46,6 +46,22 @@
     size = 32;
   };
 
+  gtk = {
+    enable = true;
+    gtk3.extraConfig.gtk-decoration-layout = "menu:";
+    theme = {
+      name = "Nordic";
+      package = pkgs.nordic;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+    };
+    cursorTheme = {
+      name = "Nordzy-cursors";
+      package = pkgs.nordzy-cursor-theme;
+    };
+  };
+
   home.sessionVariables.GTK_THEME = "Nordic";
 
   # Add programs
