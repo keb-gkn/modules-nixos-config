3469d6ea2a53af9802c72188b7f2fded6ad52458
CloudyChris <a.c.pufu@gmail.com>
2024-07-09 08:23:51 +0300

    fixed typo

--- nixos/Potato/hardware-configuration.nix
+++ nixos/Potato/hardware-configuration.nix
@@ -103,5 +103,5 @@
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmare;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
