7c6070ef9ac1fe536ff2be381dcd37fc9b6c5859
CloudyChris <a.c.pufu@gmail.com>
2024-08-02 22:13:10 +0300

    changed ocr to an explicit file and desktop entry duo

--- home-manager/programs/ocr.nix
+++ home-manager/programs/ocr.nix
@@ -1,11 +1,18 @@
 {pkgs, ...}: {
+  home.file = {
+    ".local/bin/ocr" = {
+      source = "${pkgs.writeScript "ocr" ''
+        #!/usr/bin/env bash
+        ${pkgs.xfce.xfce4-screenshooter}/bin/xfce4-screenshooter -r --save /tmp/ocr-tmp.png
+        ${pkgs.tesseract}/bin/tesseract /tmp/ocr-tmp.png /tmp/ocr-out
+        cat /tmp/ocr-out.txt | ${pkgs.xclip}/bin/xclip -sel clip
+        rm /tmp/ocr-tmp.png
+      ''}";
+    };
+  };
   xdg.desktopEntries.ocr = {
     name = "OCR image";
-    exec = "${pkgs.writeScript "ocr" ''
-      ${pkgs.xfce.xfce4-screenshooter}/bin/xfce4-screenshooter -r --save /tmp/ocr-tmp.png
-      ${pkgs.tesseract}/bin/tesseract /tmp/ocr-tmp.png /tmp/ocr-out
-      cat /tmp/ocr-out.txt | ${pkgs.xclip}/bin/xclip -sel clip
-      rm /tmp/ocr-tmp.png
-    ''}";
+    exec = "~/.local/bin/ocr";
+    executable = true;
   };
 }
