3fdbde3ab5d59a0d142b229c465c84a7851b4d4d
CloudyChris <a.c.pufu@gmail.com>
2024-09-24 15:19:55 +0300

    undistractMe was not a home-manager option for bash

--- home-manager/programs/bash.nix
+++ home-manager/programs/bash.nix
@@ -8,10 +8,5 @@
       nosrs = "sudo nixos-rebuild switch --flake /etc/nixos#AWESIMOV";
       hmrs = "home-manager switch --flake /etc/nixos#arthank@AWESIMOV";
     };
-    undistractMe = {
-      enable = true;
-      playSound = false;
-      timeout = 60;
-    };
   };
 }
