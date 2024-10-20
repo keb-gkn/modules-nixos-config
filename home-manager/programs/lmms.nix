0265b227f2a79b8767e365cd5722ca197a3849ba
CloudyChris <a.c.pufu@gmail.com>
2024-10-20 23:41:43 +0300

    lmms is now downloaded not copied

--- home-manager/programs/lmms.nix
+++ home-manager/programs/lmms.nix
@@ -1,10 +1,18 @@
 {
-  home.file."/home/arthank/.appimages/lmms-1.2.2.AppImage" = {
-    source = ../llms/lmms-1.2.2.AppImage;
+  home.file."/home/arthank/.appimages/.dummy" = {
+    text = "dummy";
     recursive = true;
-  };
-
-  home.file."/home/arthank/.local/share/applications/lmms.desktop" = {
-    source = ../llms/llms.desktop;
+    onChange = ''
+      if test -f "/home/arthank/.appimages/lmms-1.2.2.AppImage"; then
+        echo "LMMS already downloaded"
+      else
+        curl -o /home/arthank/.appimages/lmms-1.2.2.AppImage https://github.com/LMMS/lmms/releases/download/v1.2.2/lmms-1.2.2-linux-x86_64.AppImage
+        chmod +x /home/arthank/.appimages/lmms-1.2.2.AppImage
+      fi
+      if test -f "/home/arthank/.local/share/applications/lmms.desktop"; then
+        cp /etc/nixos/home-manager/lmms/lmms.desktop /home/arthank/.local/share/applications/lmms.desktop
+        chmod +x /home/arthank/.local/share/applications/lmms.desktop
+      fi
+    '';
   };
 }
