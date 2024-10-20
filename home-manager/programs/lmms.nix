3ffbbe66cb7c86b64f80cef812c93c1391fd15cb
CloudyChris <a.c.pufu@gmail.com>
2024-10-21 01:42:33 +0300

    changed curl with wget cause curl sucks

--- home-manager/programs/lmms.nix
+++ home-manager/programs/lmms.nix
@@ -5,30 +5,30 @@
   ...
 }: {
   home.activation.lmmsInstallScript = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    if test -d "${config.home.homeDirectory}/.appimages/"; then
+    if [[ -d "${config.home.homeDirectory}/.appimages/" ]]; then
       echo ".appimages dir exists"
     else
-      if [\$\{DRY_RUN+x}]; then
+      if [[ -v DRY_RUN ]]; then
         echo "mkdir ${config.home.homeDirectory}/.appimages"
       else
         mkdir ${config.home.homeDirectory}/.appimages
       fi
     fi
-    if test -f "${config.home.homeDirectory}/.appimages/lmms-1.2.2.AppImage"; then
+    if [[ -f "${config.home.homeDirectory}/.appimages/lmms-1.2.2.AppImage" ]]; then
       echo "LMMS already downloaded"
     else
-      if [\$\{DRY_RUN+x}]; then
+      if [[ -v DRY_RUN ]]; then
         echo "${pkgs.curl}/bin/curl -o ${config.home.homeDirectory}/.appimages/lmms-1.2.2.AppImage http://github.com/LMMS/lmms/releases/download/v1.2.2/lmms-1.2.2-linux-x86_64.AppImage"
         echo "chmod +x ${config.home.homeDirectory}/.appimages/lmms-1.2.2.AppImage"
       else
-        ${pkgs.curl}/bin/curl -o ${config.home.homeDirectory}/.appimages/lmms-1.2.2.AppImage http://github.com/LMMS/lmms/releases/download/v1.2.2/lmms-1.2.2-linux-x86_64.AppImage
+        ${pkgs.wget}/bin/wget -O ${config.home.homeDirectory}/.appimages/lmms-1.2.2.AppImage http://github.com/LMMS/lmms/releases/download/v1.2.2/lmms-1.2.2-linux-x86_64.AppImage
         chmod +x ${config.home.homeDirectory}/.appimages/lmms-1.2.2.AppImage
       fi
     fi
-    if test -f "${config.home.homeDirectory}/.local/share/applications/lmms.desktop"; then
+    if [[ -f "${config.home.homeDirectory}/.local/share/applications/lmms.desktop" ]]; then
       echo "LMMS desktop file already moved to share/applications"
     else
-      if [\$\{DRY_RUN+x}]; then
+      if [[ -v DRY_RUN ]]; then
         echo "cp /etc/nixos/home-manager/lmms/lmms.desktop ${config.home.homeDirectory}/.local/share/applications/lmms.desktop"
         echo "chmod +x ${config.home.homeDirectory}/.local/share/applications/lmms.desktop"
       else
