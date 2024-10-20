8185719e096b5c870310ea80b80556032aa651ce
CloudyChris <a.c.pufu@gmail.com>
2024-10-21 01:26:01 +0300

    checking if dry run is set

--- home-manager/programs/lmms.nix
+++ home-manager/programs/lmms.nix
@@ -8,7 +8,7 @@
     if test -d "${config.home.homeDirectory}/.appimages/"; then
       echo ".appimages dir exists"
     else
-      if [$DRY_RUN]; then
+      if [\$\{DRY_RUN+x}]; then
         echo "mkdir ${config.home.homeDirectory}/.appimages"
       else
         mkdir ${config.home.homeDirectory}/.appimages
@@ -17,7 +17,7 @@
     if test -f "${config.home.homeDirectory}/.appimages/lmms-1.2.2.AppImage"; then
       echo "LMMS already downloaded"
     else
-      if [$DRY_RUN]; then
+      if [\$\{DRY_RUN+x}]; then
         echo "${pkgs.curl}/bin/curl -o ${config.home.homeDirectory}/.appimages/lmms-1.2.2.AppImage http://github.com/LMMS/lmms/releases/download/v1.2.2/lmms-1.2.2-linux-x86_64.AppImage"
         echo "chmod +x ${config.home.homeDirectory}/.appimages/lmms-1.2.2.AppImage"
       else
@@ -28,7 +28,7 @@
     if test -f "${config.home.homeDirectory}/.local/share/applications/lmms.desktop"; then
       echo "LMMS desktop file already moved to share/applications"
     else
-      if [$DRY_RUN]; then
+      if [\$\{DRY_RUN+x}]; then
         echo "cp /etc/nixos/home-manager/lmms/lmms.desktop ${config.home.homeDirectory}/.local/share/applications/lmms.desktop"
         echo "chmod +x ${config.home.homeDirectory}/.local/share/applications/lmms.desktop"
       else
