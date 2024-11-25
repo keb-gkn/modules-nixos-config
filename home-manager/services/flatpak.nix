9dec3e04235ffdde596475ff3000e503e914aca8
CloudyChris <a.c.pufu@gmail.com>
2024-11-25 22:34:09 +0200

    Trying flatpak again

--- home-manager/services/flatpak.nix
+++ home-manager/services/flatpak.nix
@@ -1,7 +1,11 @@
-{inputs, ...}: {
+{ inputs, lib, ... }: {
   imports = [inputs.nix-flatpak.homeManagerModules.nix-flatpak];
 
   services.flatpak = {
     enable = true;
+    remotes = lib.mkOptionDefault [{
+      name = "flathub-beta";
+      location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+    }];
   };
 }
