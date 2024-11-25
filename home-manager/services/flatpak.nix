81bf9614dfcb598138701d09e64fef3f53decb38
CloudyChris <a.c.pufu@gmail.com>
2024-11-25 22:44:26 +0200

    Added sober flatpak

--- home-manager/services/flatpak.nix
+++ home-manager/services/flatpak.nix
@@ -7,5 +7,11 @@
       name = "flathub-beta";
       location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
     }];
+    packages = [
+      {
+        flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
+        sha256 = "sha256:1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
+      }
+    ];
   };
 }
