8657d118ac392d213e7a6e0daa725eb915137ed4
CloudyChris <a.c.pufu@gmail.com>
2024-11-29 20:40:30 +0200

    Tinkered with pipewire and bluetooth, changed from pavucontrol to pwvucontrol

--- home-manager/programs/audio.nix
+++ home-manager/programs/audio.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    pavucontrol
+    pwvucontrol
     easyeffects
     noisetorch
   ];
--- nixos/modules/pipewire.nix
+++ nixos/modules/pipewire.nix
@@ -11,11 +11,14 @@
       enable = config.services.pipewire.enable;
       extraConfig = {
         bluetoothEnhancements = {
-            "monitor.bluez.properties" = {
+          "support.dbus" = true;
+          "support.reserve-device" = true;
+          "support.portal-premissionstore" = true;
+          "monitor.bluez.properties" = {
             "bluez5.enable-sbc-xq" = true;
             "bluez5.enable-msbc" = true;
             "bluez5.enable-hw-volume" = true;
-            "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+            "bluez5.roles" = [ "a2dp_sink" "a2dp_source" "bap_sink" "bap_source" "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
           };
         };
       };
