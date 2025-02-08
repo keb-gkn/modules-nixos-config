4465f8c894f371f2dc351724a97544dc93debf57
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-08 22:11:37 +0100

    Create vm.nix

---
+++ nixos/modules/vm.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+
+# QEMU
+  programs.virt-manager.enable = true;
+  virtualisation.libvirtd.enable = true;
+  services.qemuGuest.enable = true;
+
+ # VMware
+  virtualisation.vmware.host.enable = true;
+
+  virtualisation.vmware.guest = {
+    enable = true;
+    headless = true;
+  };
+}
