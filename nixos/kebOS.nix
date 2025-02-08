204994391d3eb8f62c2fc2729c12068af255f398
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-08 19:49:02 +0100

    change name

---
+++ nixos/kebOS.nix
@@ -0,0 +1,36 @@
+{
+  imports = [
+    # base config with pkgs and services
+    ./modules/configuration.nix
+    ./users/arthank.nix
+
+    # modules
+    ./modules/pipewire.nix
+    ./modules/bluetooth.nix
+    ./modules/printing.nix
+    ./modules/gaming.nix
+    ./modules/appimage.nix
+    ./modules/vm.nix
+    ./modules/tor.nix
+    ./modules/networking.nix 
+    ./modules/ssh.nix
+    ./modules/printing.nix
+    ./modules/firewall.nix
+    ./modules/ollama.nix  
+    ./modules/firefox.nix
+    ./modules/packages.nix
+    ./modules/optimisation.nix
+    ./modules/kde.nix
+    ./modules/boot.nix
+    
+
+    # hardware
+    ./hardware/machines/hardware-configuration.nix
+    
+
+    # services
+    ./services/flatpak.nix
+  ];
+
+  # networking.hostName = "kebOS";
+}
