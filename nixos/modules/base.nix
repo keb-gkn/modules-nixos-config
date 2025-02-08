8a22c5db4f1d0e12bf0aa15c40b25839e63101ad
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-08 21:39:38 +0100

    Update base.nix

--- nixos/modules/base.nix
+++ nixos/modules/base.nix
@@ -45,85 +45,11 @@
     })
     config.nix.registry;
 
-  nix.settings = {
-    # Enable flakes and new 'nix' command
-    experimental-features = "nix-command flakes";
-    # Deduplicate and optimize nix store
-    auto-optimise-store = true;
-  };
-
-  # Required for SDDM
-  qt.style = "breeze";
-
-  security = {
-    polkit = {
-      enable = true;
-    };
-    rtkit = {
-      enable = true;
-    };
-  };
-
-  services = {
-    libinput.mouse.middleEmulation = false;
-    xserver = {
-      enable = true;
-      xkb = {
-        layout = "ro";
-        model = "p105+inet";
-        variant = "";
-        options = "lv3:ralt_switch, compose:sclk, grp:menu_switch";
-      };
-    };
-    displayManager = {
-      sddm = {
-        enable = true;
-        theme = "nord";
-      };
-    };
-  };
-
-  # portals for flatpaks and sandboxed apps
-  xdg.portal = {
-    enable = true;
-    xdgOpenUsePortal = false;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-kde
-    ];
-    config = {
-      common = {
-        default = [
-          "gtk"
-        ];
-        "org.freedesktop.impl.portal.Secret" = [
-          "gnome-keyring"
-        ];
-      };
-    };
-  };
-
-  # Enable internet
-  networking = {
-    networkmanager = {
-      enable = true;
-      plugins = with pkgs; [
-        networkmanager-openvpn
-      ];
-    };
-  };
-
-  # Enable hardware acceleration
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
   # Set your time zone.
-  time.timeZone = "Europe/Bucharest";
+  time.timeZone = "Europe/Paris";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "fr_FR.UTF-8";
   console = {
     earlySetup = true;
     font = "${pkgs.kbd}/share/consolefonts/Lat2-Terminus16.psfu.gz";
@@ -131,7 +57,6 @@
       terminus_font
       kbd
     ];
-    useXkbConfig = true;
   };
 
   # My traceroute
@@ -159,36 +84,8 @@
     enableZshIntegration = true;
   };
 
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Use keys only. Remove if you want to SSH using password (not recommended)
-      PasswordAuthentication = false;
-    };
-  };
-
   environment.systemPackages = with pkgs; [
-    # build tools & dev
-    gnumake
-    cmake
-    binutils
-    libgcc
-    libclang
-    scons
-    (python3.withPackages (ps:
-      with ps; [
-        pip
-        requests
-        xlib
-      ]))
-    lua54Packages.lua
-    lua54Packages.luarocks-nix
-    stylua
-
+   
     # shells
     fish
 
@@ -204,73 +101,19 @@
     # cursors
     nordzy-cursor-theme
 
-    # utils
-    pciutils
-    toybox
-
-    # lua custom packages
-    extraLuaPackages.dbus_proxy
-    extraLuaPackages.enum
-    extraLuaPackages.pulseaudio_dbus
-    extraLuaPackages.upower_dbus
-
-    # power management
-    upower
-    xfce.xfce4-power-manager
-
-    # audio / control
-    playerctl
-
-    # xorg
-    xclip
-    xorg.xrdb
-    xorg.xfs
-    xorg.xfd
-    xorg.xwd
-    xorg.xpr
-    xorg.xsm
-    xorg.xev
-    xorg.xwud
-    xorg.xset
-    xorg.xmag
-    xorg.luit
-    xorg.xprop
-    xorg.xmore
-    xorg.xload
-    #xorg.xkill
-    #xorg.xinit
-    #xorg.xhost
-    xorg.xrandr
-    xorg.imake
-    xorg.xvinfo
-    xorg.xinput
-    xorg.xgamma
-    xorg.xcmsdb
-    xorg.xkbevd
-    xorg.xmodmap
-    xorg.xkbcomp
-    xorg.xfsinfo
-    xorg.viewres
-    xorg.smproxy
-    xorg.xstdcmap
-    xorg.xsetroot
-    xorg.xrefresh
-    xorg.xmessage
-    xorg.xkbprint
-
-    # settings
-    xsettingsd
-
     # sddm
     sddm-themes.nord
-    libsForQt5.full
-    libsForQt5.plasma-framework
-    libsForQt5.breeze-qt5
-    libsForQt5.breeze-icons
+
   ];
 
   environment.sessionVariables.TERMINAL = "alacritty";
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "24.11";
+ # SSD 
+  services.fstrim.enable = true;
+  fileSystems."home/kebzcool/Saves" =
+    { device = "/dev/disk/by-uuid/6b33f298-ad54-4671-bae7-da34725fafd0";
+      fsType = "ext4";
+  };
+
+  system.stateVersion = "24.11"; # Did you read the comment?
 }
