43e2ba3813c96f41c49ff58064cd8d9750d6304d
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-08 21:09:36 +0100

    my config pipewire.nix

--- nixos/modules/pipewire.nix
+++ nixos/modules/pipewire.nix
@@ -1,31 +1,14 @@
 { pkgs, config, ... }:
-{
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-    wireplumber = {
-      enable = config.services.pipewire.enable;
-      extraConfig = {
-        bluetoothEnhancements = {
-          "support.dbus" = true;
-          "support.reserve-device" = true;
-          "support.portal-premissionstore" = true;
-          "monitor.bluez.properties" = {
-            "bluez5.enable-sbc-xq" = true;
-            "bluez5.enable-msbc" = true;
-            "bluez5.enable-hw-volume" = true;
-            "bluez5.roles" = [ "a2dp_sink" "a2dp_source" "bap_sink" "bap_source" "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
-          };
-        };
-      };
-    };
-  };
 
-  environment.systemPackages = with pkgs; [
-    pulseaudio
-  ];
+{
+  # Enable sound with pipewire.
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+      jack.enable = true;
+    };
 }
