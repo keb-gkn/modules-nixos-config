e6fcf4e68136a798179f17be67b9d06e962668e1
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-09 11:02:36 +0100

    Update and rename optimisation.nix to system.nix

--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -8,6 +8,27 @@
   # zRAM Configuration
    zramSwap.enable = true;
 
+   hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        intel-gpu-tools
+        intel-media-driver
+        vaapiIntel
+        vaapiVdpau
+        libvdpau-va-gl
+        libva
+      ];
+      extraPackages32 = with pkgs; [
+        intel-gpu-tools
+        intel-media-driver
+        vaapiIntel
+        vaapiVdpau
+        libvdpau-va-gl
+        libva
+      ];
+    };
+
   # Automatic Garbage collection
    nix.gc = {
      automatic = true;
