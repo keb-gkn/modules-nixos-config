8cb2f47a9dd742d370f7ae3b1dbe9e1e06da2f88
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-09 17:14:47 +0100

    Update kde.nix

--- nixos/modules/kde.nix
+++ nixos/modules/kde.nix
@@ -2,9 +2,6 @@
 
 {
 
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
   # Enable the KDE Plasma Desktop Environment.
   services = {
     displayManager.sddm.enable = true;
