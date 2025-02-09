0110d53085da260b5958a6810b8168cc35c0a658
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-09 16:56:44 +0100

    del font terminus

--- nixos/modules/configuration.nix
+++ nixos/modules/configuration.nix
@@ -52,13 +52,8 @@
 
  # Select internationalisation properties.
   i18n.defaultLocale = "fr_FR.UTF-8";
-  console = {
-    earlySetup = true;
-    font = "${pkgs.kbd}/share/consolefonts/Lat2-Terminus16.psfu.gz";
-    packages = with pkgs; [
-      terminus_font
-   ];
  };
+
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "fr_FR.UTF-8";
     LC_IDENTIFICATION = "fr_FR.UTF-8";
