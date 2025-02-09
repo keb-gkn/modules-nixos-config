d46f1b471ce52266ef3dac6317a07c17871d9671
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-09 10:31:18 +0100

    Update git.nix

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -2,7 +2,7 @@
   programs.git = {
     enable = true;
     lfs.enable = true;
-    userEmail = "a.c.pufu@gmail.com";
-    userName = "CloudyChris";
+    userEmail = "";
+    userName = "";
   };
 }
