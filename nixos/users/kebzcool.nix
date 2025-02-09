1088dc715d9fcc78763f0959b24dc0398a02c4d3
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-09 12:46:13 +0100

    Clean user account

--- nixos/users/kebzcool.nix
+++ nixos/users/kebzcool.nix
@@ -1,16 +1,8 @@
 {
-  users.users = {
-    kebzcool = {
-      isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      ];
-
-      extraGroups = [
-        "wheel"
-        "networkmanager"
-        "render"
-      ];
-    };
+  # User account
+   users.users.kebzcool = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" "render" ];
+    uid = 1000;
   };
 }
