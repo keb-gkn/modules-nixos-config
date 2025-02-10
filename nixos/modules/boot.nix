2d6406120d1949b1de8d09f949081cf00f49f607
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-10 21:20:39 +0100

    amd_pstate active

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -7,15 +7,7 @@
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
+    kernelParams = [ "amd_pstate=active" ];
 
     # Configuration de Plymouth
     plymouth = {
