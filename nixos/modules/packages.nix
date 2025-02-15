7c58cf36c66d05ea17b378e591bae08749e4f2ee
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-15 18:51:03 +0100

    add app compression

--- nixos/modules/packages.nix
+++ nixos/modules/packages.nix
@@ -12,39 +12,72 @@
    # App
    kdePackages.kate
    kdePackages.kcalc
+
    #Utils
    cool-retro-term 
    bottles
    ntfs3g # support NTFS
+
    # Montage video
    kdenlive
    davinci-resolve 
+
    # Security
    bitwarden 
+
    # Fetch
    fastfetch   
+
    # Photos and image
    gimp
    inkscape
    darktable
    upscayl 
    krita
+
    # Médias
    vlc
    freetube
    spotube
+
    # Bureautique
    libreoffice-fresh
    hunspell
    hunspellDicts.fr-moderne
    onlyoffice-desktopeditors
    joplin-desktop
+
    # Navigateur
    brave
+
    # Messagerie
    thunderbird 
    discord
+
    # Sync
    git
+
+   # Compression 
+   arj
+   brotli
+   bzip2
+   cpio
+   gnutar
+   gzip
+   lha
+   libarchive
+   lrzip
+   lz4
+   lzop
+   p7zip
+   pbzip2
+   pigz
+   pixz
+   unrar
+   unzip
+   xz
+   zip
+   zstd
+     
  ];
 }
