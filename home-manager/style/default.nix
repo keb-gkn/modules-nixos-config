a88e6c52715f46482725f0cd2993af2e1b27004a
kebzcool <157246690+KebZcool@users.noreply.github.com>
2025-03-27 21:05:51 +0100

    Add cursor nordic theme

--- home-manager/style/default.nix
+++ home-manager/style/default.nix
@@ -9,6 +9,15 @@
 
   fonts.fontconfig.enable = true;
 
+home = {
+    pointerCursor = {
+      name = "nordic-cursors";
+      package = pkgs.nordic-cursor-theme;
+      x11.enable = true;
+      gtk.enable = true;
+      size = 32;
+    };
+
     home.file = {
       "${config.home.homeDirectory}/.face" = {
         source = ../assets/user-icon.png;
