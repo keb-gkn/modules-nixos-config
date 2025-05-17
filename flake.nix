b0c6c637d52ebe057f05bb554b47735b8a5e0743
KebZman <157246690+KebZcool@users.noreply.github.com>
2025-05-17 11:20:22 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -65,7 +65,7 @@
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
           # > Our main home-manager configuration file <
-          ./home-manager/kebzcool_kebOS.nix
+          ./home-manager/home_kebOS.nix
         ];
       };
     };
