452b3eef35f4d37b883ff24316f41f37c0ca5c78
Gen-Kn <157246690+KebZcool@users.noreply.github.com>
2025-05-18 18:45:27 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      "kebzcool@kebOS" = home-manager.lib.homeManagerConfiguration {
+      "Gen-Kn@GKNOS" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
