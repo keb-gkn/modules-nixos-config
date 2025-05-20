ba76c28acf6b29e5d21f45e7ec1eee66015d5ca4
Gen-Kn <157246690+KebZcool@users.noreply.github.com>
2025-05-20 20:53:07 +0200

    add url kernel cachyOS

--- flake.nix
+++ flake.nix
@@ -12,6 +12,8 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # Kernel
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
   };
 
   outputs = {
