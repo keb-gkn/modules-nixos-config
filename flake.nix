8c9d3c7edda2595e1475c4424a41dc96d21e31f3
Gen-Kn <157246690+KebZcool@users.noreply.github.com>
2025-05-20 21:16:48 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -12,15 +12,12 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    # Kernel
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
   };
 
   outputs = {
     self,
     nixpkgs,
     home-manager,
-    chaotic,
     ...
   } @ inputs: let
     inherit (self) outputs;
