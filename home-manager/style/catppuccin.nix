076dc89b1ac455acb02a6ff7af4565b162ab1621
Gen-Kn <157246690+Gen-Kn@users.noreply.github.com>
2025-05-23 18:04:13 +0200

    Update catppuccin.nix

--- home-manager/style/catppuccin.nix
+++ home-manager/style/catppuccin.nix
@@ -1,4 +1,30 @@
 {
+
+  catppuccin = {
+    enable = true;
+    flavor = "frappe";
+   };
+
+  catppuccin.pointerCursor = {
+    enable = true;
+    flavor = "frappe";
+   };
+
+  colorschemes.catppuccin = {
+      enable = true;
+      settings = {
+        flavor = "frappe";
+        integrations = {
+          notify = true;
+          neotree = true;
+          treesitter = true;
+          treesitter_context = true;
+          native_lsp = {
+            enabled = true;
+            inlay_hints = {
+              background = true;
+            };
+
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
