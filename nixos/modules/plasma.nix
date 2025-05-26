db2395b767aef70b8dde007dc22fa5bee69753b1
Gen-Kn <157246690+Gen-Kn@users.noreply.github.com>
2025-05-26 18:53:07 +0200

    Update plasma.nix

--- nixos/modules/plasma.nix
+++ nixos/modules/plasma.nix
@@ -2,50 +2,18 @@
 
 {
 
- # sddm-astronaut-theme
-    services.displayManager.sddm = {
-      enable = true;
-      theme = "sddm-astronaut-theme"; # Must match package name
-      # settings = {
-      #   General = {
-      #     HeaderText = "Hi";
-      #     Background = "/etc/nixos/ressources/wallpapers/ghost_in_the_shell.jpg";
-      #   };
-      # };
-      extraPackages = with pkgs; [
-        kdePackages.qtmultimedia
-        kdePackages.qtsvg
-        kdePackages.qtvirtualkeyboard
-      ];
-    };
-
-  environment.systemPackages = with pkgs; [
-      (pkgs.callPackage ../../../ressources/sddm-astronaut-theme {
-        # theme = "hyprland_kath";
-        # theme = "black_hole";
-        # theme = "japanese_aesthetic";
-          theme = "astronaut";
-        # theme = "purple_leaves";
-        # theme = "pixel_sakura(-static)";
-        themeConfig = {
-          General = {
-            HeaderText = "Hi Bitch!";
-            Background = "/home/gkn/.dotfiles/nixos/ressources/wallpapers/ghost_in_the_shell.jpg";
-            FullBlur = "false";
-           };   
-        };
-     })
-  ];
-  desktopManager.plasma6.enable = true;
+  # Enable the KDE Plasma Desktop Environment.
+  services = {
+   #displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
   };
 
-  documentation.nixos.enable = false;
+   documentation.nixos.enable = false;
 
-  # Kdeconnect
-  programs.kdeconnect.enable = true;
+   # Kdeconnect
+   programs.kdeconnect.enable = true;
 
-  environment = {
-    plasma6.excludePackages = [ pkgs.kdePackages.discover ];
-  };
-
+   environment = {
+     plasma6.excludePackages = [ pkgs.kdePackages.discover ];
+   };
 }
