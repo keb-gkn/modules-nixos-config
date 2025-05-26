a242bb539b1ea69d94722d9a6104383e49c4f19b
Gen-Kn <157246690+Gen-Kn@users.noreply.github.com>
2025-05-26 18:56:04 +0200

    Update sddm-astronaut-theme.nix

--- nixos/modules/sddm-astronaut-theme.nix
+++ nixos/modules/sddm-astronaut-theme.nix
@@ -7,7 +7,7 @@
       # settings = {
       #   General = {
       #     HeaderText = "Hi";
-      #     Background = "/etc/nixos/ressources/wallpapers/1359465.png";
+      #     Background = "/etc/nixos/ressources/wallpapers/";
       #   };
       # };
       extraPackages = with pkgs; [
@@ -28,49 +28,10 @@
         themeConfig = {
           General = {
             HeaderText = "Hi Bitch!";
-            # Background = "/home/maike/.config/nixos/ressources/wallpapers/1359465.png";
+            # Background = "/home/maike/.dofiles/nixos/ressources/wallpapers/";
             Background = "${config.stylix.image}";
             FullBlur = "false";
           };
-          Colors = {
-            HeaderTextColor = config.lib.stylix.colors.base05;
-            # DateTextColor = config.lib.stylix.colors.base0A;
-            # TimeTextColor = config.lib.stylix.colors.base0B;
-            #
-            # FormBackgroundColor = config.lib.stylix.colors.base01;
-            BackgroundColor = "#fde086";
-            DimBackgroundColor = "#fde086";
-            #
-            LoginFieldBackgroundColor = "#fde086";
-            PasswordFieldBackgroundColor = "#fde086";
-            # LoginFieldTextColor = config.lib.stylix.colors.base05;
-            # PasswordFieldTextColor = config.lib.stylix.colors.base05;
-            # UserIconColor = config.lib.stylix.colors.base0C;
-            # PasswordIconColor = config.lib.stylix.colors.base0C;
-            #
-            # PlaceholderTextColor = config.lib.stylix.colors.base03;
-            # WarningColor = config.lib.stylix.colors.base08;
-            #
-            # LoginButtonTextColor = config.lib.stylix.colors.base07;
-            # # LoginButtonBackgroundColor = config.lib.stylix.colors.accent;
-            # SystemButtonsIconsColor = config.lib.stylix.colors.base0C;
-            # SessionButtonTextColor = config.lib.stylix.colors.base0C;
-            # VirtualKeyboardButtonTextColor = config.lib.stylix.colors.base0C;
-            #
-            # DropdownTextColor = config.lib.stylix.colors.base07;
-            # # DropdownSelectedBackgroundColor = config.lib.stylix.colors.accent;
-            # DropdownBackgroundColor = config.lib.stylix.colors.base01;
-            #
-            # HighlightTextColor = config.lib.stylix.colors.base01;
-            HighlightBackgroundColor = "#fde086";
-            # HighlightBorderColor = config.lib.stylix.colors.accent;
-            #
-            # HoverUserIconColor = config.lib.stylix.colors.accent;
-            # HoverPasswordIconColor = config.lib.stylix.colors.accent;
-            # HoverSystemButtonsIconsColor = config.lib.stylix.colors.accent;
-            # HoverSessionButtonTextColor = config.lib.stylix.colors.accent;
-            # HoverVirtualKeyboardButtonTextColor = config.lib.stylix.colors.accent;
-          };   
         };
      })
   ];
