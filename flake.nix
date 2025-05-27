ae0036301220659ce8de5f9e0739bc46667873f9
Gen-Kn <157246690+Gen-Kn@users.noreply.github.com>
2025-05-27 17:28:14 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -15,8 +15,6 @@
     # catppuccin flake
     catppuccin.url = "github:catppuccin/nix";
     utils.url = "github:numtide/flake-utils";
-
-    sddm-astronaut-theme.url = "./ressources/sddm-astronaut-theme";
   };
 
   outputs = {
