33357f9d82d930fa8b09e37bf75c152840cc38fb
Gen-Kn <157246690+Gen-Kn@users.noreply.github.com>
2025-05-27 17:50:11 +0000

    Update default.nix

--- nixos/assets/default.nix
+++ nixos/assets/default.nix
@@ -1,11 +1,8 @@
-let
-  dir = "/etc/nixos/common/wallpapers"; 
-in
-[
-    "${dir}/abandoned_satellite_dishes.jpg"
-    "${dir}/escaping_the_well.jpg"
-    "${dir}/fedex_cargo_ship.jpg"
-    "${dir}/ghost_in_the_shell.jpg"
-    "${dir}/the_neon_shallows.png"
-    "${dir}/the_wormworld_saga_edited.jpg"
-]
+{
+  pkgs,
+  config,
+  ...
+}: {
+  imports = [
+   ./assets/wallpapers/default.nix
+  ];
