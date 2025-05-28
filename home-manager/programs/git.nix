ef802f40a6cccd5f79cd86df47fe947cade03879
Keb_gKn <157246690+Gen-Kn@users.noreply.github.com>
2025-05-28 17:15:06 +0000

    Update git.nix

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -2,7 +2,7 @@
   programs.git = {
     enable = true;
     lfs.enable = true;
-    userName = "Gen-Kn";
+    userName = "Keb_gkn";
     userEmail = "157246690-Gen-Kn-users.noreply.github.com";
     extraConfig = {
       init = {
