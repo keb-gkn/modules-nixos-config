0b853855cb6b808dd6375cf77952a6cb4270b420
Gen-Kn <157246690+Gen-Kn@users.noreply.github.com>
2025-05-28 17:02:02 +0000

    Update plasma.nix

--- nixos/modules/plasma.nix
+++ nixos/modules/plasma.nix
@@ -1,29 +1,32 @@
 { lib, config, pkgs, ... }:
 
 {
-
   # Enable the KDE Plasma Desktop Environment.
   services = {
     displayManager.sddm.enable = true;
     desktopManager.plasma6.enable = true;
   };
 
-   documentation.nixos.enable = false;
+  documentation.nixos.enable = false;
 
-   # Kdeconnect
-   programs.kdeconnect.enable = true;
+  # Kdeconnect
+  programs.kdeconnect.enable = true;
 
-   environment.systemPackages = with pkgs; [
+  # System packages and theme configuration
+  environment.systemPackages = with pkgs; [
     catppuccin-kde
   ];
 
-     environment = {
-      plasma6.excludePackages = [ pkgs.kdePackages.discover ];
-      systemPackages = [
-        (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
-          [General]
-          background=/home/gkn/.dotfiles/nixos/assets/wallpapers/ghost_in_the_shell.jpg
-        '')
-       ];
-    };
- }
+  environment = {
+    # Exclude discover package from Plasma 6
+    plasma6.excludePackages = [ pkgs.kdePackages.discover ];
+    
+    # Configure SDDM theme with custom wallpaper
+    systemPackages = [
+      (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
+        [General]
+        background=/home/gkn/.dotfiles/nixos/assets/wallpapers/ghost_in_the_shell.jpg
+      '')
+    ];
+  };
+}
