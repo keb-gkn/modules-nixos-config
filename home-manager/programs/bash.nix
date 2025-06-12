a8805bb3e709c6d3ad2c34750c0209ce4a08a46b
Keb_gKn <157246690+keb-gkn@users.noreply.github.com>
2025-06-12 19:10:33 +0200

    Update bash.nix

--- home-manager/programs/bash.nix
+++ home-manager/programs/bash.nix
@@ -5,8 +5,8 @@
     shellAliases = {
       l = "eza";
       ll = "eza -alh";
-      norse = "sudo nixos-rebuild switch --flake /etc/nixos#gknos";
-      horse = "home-manager switch --flake /etc/nixos#gkn@gknos";
+      norse = "sudo nixos-rebuild switch --flake /home/gkn/.dotfiles/nixos#gknos";
+      horse = "home-manager switch --flake /home/gkn/.dotfiles/nixos#gkn@gknos";
     };
   };
 }
